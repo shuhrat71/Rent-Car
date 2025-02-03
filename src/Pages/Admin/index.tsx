dd930af4edae9c1e424d62f5e62693397a75b323
shuhrat7 <toshxujayivshuhrat7@gmail.com>
2025-02-03 16:54:05 +0500

    ♻️ Refactor Admin page to include archive data display with a new table layout and enhance data fetching logic

--- src/Pages/Admin/index.tsx
+++ src/Pages/Admin/index.tsx
@@ -12,8 +12,7 @@ import {
   Container,
 } from "@mui/material";
 import { ContainerTap } from "./Components";
-import ArxivPage from "./Components/arxiv";
-import CardDetailARX from "./Components/arxiv";
+
 import { createClient } from "@supabase/supabase-js";
 import CardDetail from "Pages/Rented/inxdex";
 import { CarName } from "Pages/Home/Components/Selectors/select";
@@ -24,6 +23,16 @@ import user from "Pages/Home/Components/Selectors/img/user.svg";
 import gasStation from "Pages/Home/Components/Selectors/img/gasStation.svg";
 import { RentBtn } from "Pages/Home/Components/Card/card";
 import AddCard from "./Components/addCar";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  CircularProgress,
+} from "@mui/material";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -61,7 +70,8 @@ function a11yProps(index: number) {
 export default function VerticalTabs() {
   const [value, setValue] = React.useState(0);
   const [getData, setData] = React.useState<any>([]);
-
+  const [archiveList, setArchiveList] = React.useState<any[]>([]);
+  const [loading, setLoading] = React.useState(true);
   const fetchData = async () => {
     const supabaseUrl = "https://wdybqcunwsmveabxiekf.supabase.co";
     const supabaseKey =
@@ -81,6 +91,19 @@ export default function VerticalTabs() {
       setData(data || []);
     }
     localStorage.setItem("car data", JSON.stringify(data));
+
+    const formattedData =
+      response.data?.map((item: any) => ({
+        archiveId: item.id,
+        userName: item.Users?.name,
+        carId: item["car data"]?.id,
+        carName: item["car data"]?.name,
+        carImage: item["car data"]?.img,
+        rentedId: item.rentedLists?.id,
+      })) || [];
+    console.log(formattedData);
+
+    setArchiveList(formattedData);
   };
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
@@ -115,7 +138,55 @@ export default function VerticalTabs() {
         </Container>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <ArxivPage />
+        <TableContainer
+          component={Paper}
+          sx={{ maxWidth: 800, margin: "auto", marginTop: 5 }}
+        >
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <strong>Archive ID</strong>
+                </TableCell>
+                <TableCell>
+                  <strong>User Name</strong>
+                </TableCell>
+                <TableCell>
+                  <strong>Car ID</strong>
+                </TableCell>
+                <TableCell>
+                  <strong>Car Name</strong>
+                </TableCell>
+                <TableCell>
+                  <strong>Car Image</strong>
+                </TableCell>
+                <TableCell>
+                  <strong>Rented ID</strong>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {archiveList.map((item) => (
+                <TableRow key={item.archiveId}>
+                  <TableCell>{item.archiveId}</TableCell>
+                  <TableCell>{item.userName}</TableCell>
+                  <TableCell>{item.carId}</TableCell>
+                  <TableCell>{item.carName}</TableCell>
+                  <TableCell>
+                    <img
+                      src={item.carImage}
+                      alt={item.carName}
+                      width="50"
+                      height="30"
+                      style={{ borderRadius: "5px" }}
+                    />
+                  </TableCell>
+                  <TableCell>{item.rentedId}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </TabPanel>
       <TabPanel value={value} index={2}>
         <Container>
